1449be52f90d9360ad7f2d80dfa46a8e34497d0d
santo2927 <santo2927@gmail.com>
2021-10-16 18:05:23 +0200

    Revert "addIdea"
    This reverts commit 06e486bc0aba3186e458844fbadf6f6cb19a04d9.

--- Project/Ideality/app/src/main/java/com/example/prueba2/CrearIdea.java
+++ Project/Ideality/app/src/main/java/com/example/prueba2/CrearIdea.java
@@ -5,34 +5,17 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class CrearIdea extends AppCompatActivity {
-    Sistema s=null;
-    TextView nombre, descripcion,prioridad;
-    Button b;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-        Sistema s=Sistema.cargarSistema();
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_idea);
-        b=findViewById(R.id.button);
-        nombre=findViewById(R.id.editTextTextPersonName);
-        descripcion=findViewById(R.id.editTextTextMultiLine);
-        prioridad=findViewById(R.id.textView2);
 
         //Barra de herramientas
         Toolbar myToolbar2 = (Toolbar) findViewById(R.id.my_toolbar2);
         setSupportActionBar(myToolbar2);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                s.createIdea((String) nombre.getText(),(String) descripcion.getText(),toInt((String) prioridad.getText()));
-            }
-        });
         //fin barra
 
 
@@ -42,15 +25,5 @@ public class CrearIdea extends AppCompatActivity {
     public boolean onCreateOptionsMenu (Menu mimenuidea) {
         getMenuInflater().inflate(R.menu.menu_idea, mimenuidea);
         return true;
-    }
-
-    private Integer toInt(String s){
-        Integer i;
-        try{
-            i=Integer.parseInt(s);
-        }catch (Exception e){
-            i=0;
-        }
-        return i;
     }
 }
